932bbbd6096192a5771d83f0d20047faa5bb05bb
nikolay <galkina.s.a@gmail.com>
2016-09-03 13:31:08 +0200

    vector: const interface

--- src/vector.h
+++ src/vector.h
@@ -6,19 +6,23 @@ template <typename T> class ray_vector {
 public:
   ray_vector(T a, T b, T c);
   ray_vector();
-  void print_coordinates();
+
   T x() const;
   T y() const;
   T z() const;
-  ray_vector operator+(ray_vector another);
-  ray_vector operator-(ray_vector another);
-  T operator&(ray_vector another);
-  ray_vector operator%(ray_vector another);
-  ray_vector operator*(T number);
-  ray_vector operator^(ray_vector another);
-  bool operator==(ray_vector another);
-  double find_length();
-  ray_vector get_unit_vector();
+  ray_vector get_unit_vector() const;
+  double find_length() const;
+
+  bool operator==(ray_vector const &another) const;
+
+  ray_vector operator+(ray_vector const &another) const;
+  ray_vector operator-(ray_vector const &another) const;
+  T operator&(ray_vector const &another) const;
+  ray_vector operator%(ray_vector const &another) const;
+  ray_vector operator*(T number) const;
+  ray_vector operator^(ray_vector const &another) const;
+
+  void print_coordinates() const;
 
 private:
   T x_;
@@ -34,7 +38,7 @@ ray_vector<T>::ray_vector(T a, T b, T c) : x_(a), y_(b), z_(c) {}
 
 template <typename T> ray_vector<T>::ray_vector() : x_(0), y_(0), z_(0) {}
 
-template <typename T> void ray_vector<T>::print_coordinates() {
+template <typename T> void ray_vector<T>::print_coordinates() const {
   std::cout << "X: " << x_ << "\n"
             << "Y: " << y_ << "\n"
             << "Z: " << z_ << "\n";
@@ -48,46 +52,48 @@ template <typename T> T ray_vector<T>::y() const { return y_; }
 template <typename T> T ray_vector<T>::z() const { return z_; }
 
 template <typename T>
-ray_vector<T> ray_vector<T>::operator+(ray_vector<T> another) {
+ray_vector<T> ray_vector<T>::operator+(ray_vector<T> const &another) const {
   return ray_vector<T>(x_ + another.x_, y_ + another.y_, z_ + another.z_);
 }
 
 template <typename T>
-ray_vector<T> ray_vector<T>::operator-(ray_vector<T> another) {
+ray_vector<T> ray_vector<T>::operator-(ray_vector<T> const &another) const {
   return ray_vector<T>(x_ - another.x_, y_ - another.y_, z_ - another.z_);
 }
 
-template <typename T> T ray_vector<T>::operator&(ray_vector<T> another) {
+template <typename T>
+T ray_vector<T>::operator&(ray_vector<T> const &another) const {
   return x_ * another.x_ + y_ * another.y_ + z_ * another.z_;
 }
 
 template <typename T>
-ray_vector<T> ray_vector<T>::operator%(ray_vector<T> another) {
+ray_vector<T> ray_vector<T>::operator%(ray_vector<T> const &another) const {
   return ray_vector<T>(y_ * another.z_ - z_ * another.y_,
                        z_ * another.x_ - x_ * another.z_,
                        x_ * another.y_ - y_ * another.x_);
 }
 
 template <typename T>
-ray_vector<T> ray_vector<T>::operator^(ray_vector<T> another) {
+ray_vector<T> ray_vector<T>::operator^(ray_vector<T> const &another) const {
   return ray_vector<T>(x_ * another.x_, y_ * another.y_, z_ * another.z_);
 }
 
-template <typename T> bool ray_vector<T>::operator==(ray_vector<T> another) {
+template <typename T>
+bool ray_vector<T>::operator==(ray_vector<T> const &another) const {
   return (std::abs(x_ - another.x_) < 1e-6) &&
          (std::abs(y_ - another.y_) < 1e-6) &&
          (std::abs(z_ - another.z_) < 1e-6);
 }
 
-template <typename T> ray_vector<T> ray_vector<T>::operator*(T number) {
+template <typename T> ray_vector<T> ray_vector<T>::operator*(T number) const {
   return ray_vector<T>(x_ * number, y_ * number, z_ * number);
 }
 
-template <typename T> double ray_vector<T>::find_length() {
+template <typename T> double ray_vector<T>::find_length() const {
   return sqrt(x_ * x_ + y_ * y_ + z_ * z_);
 }
 
-template <typename T> ray_vector<T> ray_vector<T>::get_unit_vector() {
+template <typename T> ray_vector<T> ray_vector<T>::get_unit_vector() const {
   double length = find_length();
   return ray_vector<T>(x_ / length, y_ / length, z_ / length);
 }
